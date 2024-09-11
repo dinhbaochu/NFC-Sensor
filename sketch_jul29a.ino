f603db38f21d73a966284026928389abc5bef16c
dinhbaochu <101037101+dinhbaochu@users.noreply.github.com>
2024-09-11 15:17:17 +0800

    Add files via upload

--- sketch_jul29a.ino
+++ sketch_jul29a.ino
@@ -6,15 +6,14 @@ void setup(){
 ntag.begin();
 int number=0;
 number = analogRead(A0);
+number=number*330/1023;
 
-byte eepromdata[4];
+byte eepromdata[2];
 
-eepromdata[0] = 0x30+number / 1000;          // Thousands place
-eepromdata[1] = 0x30+(number / 100) % 10;    // Hundreds place
-eepromdata[2] = 0x30+(number / 10) % 10;     // Tens place
-eepromdata[3] = 0x30+number % 10;            // Units place
+eepromdata[0] = 0x30 + (number / 10) % 10;   // Tens place
+eepromdata[1] = 0x30 + number % 10;          // Units place
 
 
-ntag.writeEeprom(16*1, eepromdata, 4);}
+ntag.writeEeprom(16*1, eepromdata, 2);}
 
 void loop() {}
